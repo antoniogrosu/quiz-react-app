2774e0739ef765cff5aeb81e0c323e5d952ae4f3
Anto Gabriel <antoniowebdesign19@gamil.com>
2023-01-07 02:33:59 +0200

    Quizz screen | Preloader |

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from "react"
+export default function Question(props){
 
-export default function Question(){
 
-    //questions state
-    const [questions , setQuestions] = useState([])
-
-    //fetch the data
-    useEffect(()=>{
-        fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=boolean")
-        .then(res => res.json())
-        .then(data => setQuestions(data.results))
-    },[])
-    return questions.map( item =>(
-        <div className="question-div" style={{width: "100%"}}>
+    return props.data.map( item => (
+        <>
+        <div  id = {item.id} className="question-div" style={{width: "100%"}}>
             <h1 style={{fontFamily : "Montserrat" , color : "#293264" , fontSize : '1.25rem'}}>{item.question}</h1>
             <div style={{marginTop : "30px"}} className="buttons-div">
-                <button>True</button>
-                <button>False</button>
+                <button value="True">True</button>
+                <button value="False">False</button>
             </div>
         </div>
+        
+        </>
     ))
 }
--- src/components/QuizScreen.jsx
+++ src/components/QuizScreen.jsx
@@ -1,9 +1,40 @@
 import Question from "./Question"
+import { useEffect, useState } from "react"
+import { nanoid } from "nanoid"
+import {Sugar} from 'react-preloaders';
+
 export default function TestScreen(){
+
+
+    //questions state
+    const [data , setData] = useState([])
+
+    //loading state
+    const [loading , setLoading] = useState(true)
+
+    //fetch the data
+    useEffect(()=>{
+        fetch("https://opentdb.com/api.php?amount=5&difficulty=medium&type=boolean")
+        .then(res => res.json())
+        .then(data => setData(data.results))
+        .then(json => {
+            setLoading(false)
+        })
+    },[])    
+    
+    //add id to every object
+    function addId(){
+        const arr = questions.map(obj => ({...obj , id : nanoid()}))
+        setData(arr)
+    }
+
     return(
+        <>
         <div className="test-screen" style={{backgroundColor : "#F5F7FB" , display: "flex" , flexDirection:"column"}}>
             <h1 style={{fontFamily : "Montserrat" , color :"rgba(41, 50 , 100 , 0.35)" , width: "100%" , marginBottom : "75px" , fontSize :'1rem'}}>QUESTIONS</h1>
-            <Question />
+            <Question data ={data}/>
         </div>
+        <Sugar customLoading={loading} background="#F5F7FB" color={"#4D5B9E"} />
+        </>
     )
 }
