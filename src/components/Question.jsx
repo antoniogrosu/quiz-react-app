69492743439dd53eee4379e7a80ae2e7fd344db9
Anto Gabriel <antoniowebdesign19@gamil.com>
2023-01-15 15:42:23 +0200

    value feature 50%

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,8 +3,8 @@ export default function Question(props){
         <div  id = {item.id} className="question-div" style={{width: "100%"}}>
             <h1 style={{fontFamily : "Montserrat" , color : "#293264" , fontSize : '1.25rem'}}>{item.question}</h1>
             <div style={{marginTop : "30px"}} className="buttons-div">
-                <button value="True">True</button>
-                <button value="False">False</button>
+                <button value="True" >True</button>
+                <button value="False" >False</button>
             </div>
         </div>
     ))
--- src/components/QuizScreen.jsx
+++ src/components/QuizScreen.jsx
@@ -18,19 +18,29 @@ export default function TestScreen(){
         .then(res => res.json())
         .then(data => {
             const arr = data.results;
-            const newArr = arr.map(obj => ({...obj , id : nanoid()}))
-            setData(newArr)
+            const newArr = arr.map(obj => ({...obj , id : nanoid() , value : ""}))
+            const  dataArr = newArr.map(obj => ({corect : obj.correct_answer  , id : obj.id , question : obj.question ,  value : obj.value}))
+            setData(dataArr)
         })
         .then(json => {
             setLoading(false)
         })
-    },[])    
-         
+    },[])
+
+    // function toggleAnswer(obj ,id ,event){
+        // const value = event.target.value
+        // const index = data.indexOf(elem => elem.id == id)
+        // const newObj = {...obj , value : value}
+        // setData(data.splice(index , 1 , newObj))
+    // }
+    const obj = data[1]
+    data.splice(1 , 1 , {...obj , value:"true"})
+    console.log(data)
     return(
         <>
         <div className="test-screen" style={{backgroundColor : "#F5F7FB" , display: "flex" , flexDirection:"column"}}>
             <h1 style={{fontFamily : "Montserrat" , color :"rgba(41, 50 , 100 , 0.35)" , width: "100%" , marginBottom : "75px" , fontSize :'1rem'}}>QUESTIONS</h1>
-            <Question data ={data}/>
+            <Question data ={data} />
         </div>
         <Sugar customLoading={loading} background="#F5F7FB" color={"#4D5B9E"} />
         </>
